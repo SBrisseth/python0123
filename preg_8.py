67e2d9605979afe7f47652d983ba206c01324609
SBrisseth <110753585+SBrisseth@users.noreply.github.com>
2023-01-15 14:04:54 -0500

    subir preg_8.py a github

---
+++ preg_8.py
@@ -0,0 +1,7 @@
+print("PREGUNTA Nº8")
+password ="contraseña"
+palabra=input("ingrese la contraseña:")
+if(password==palabra.lower()):
+  print("La contraseña coincide")
+else:
+  print("La contraseña no coincide")
