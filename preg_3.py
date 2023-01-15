59969db76dbbe38a774d449ec19383397ccdefcf
SBrisseth <110753585+SBrisseth@users.noreply.github.com>
2023-01-15 14:03:17 -0500

    subir preg_3.py a github

---
+++ preg_3.py
@@ -0,0 +1,9 @@
+print("PREGUNTA Nº3")
+x = float(input("Ingrese el 1er numero :"))
+y = float(input("Ingrese el 2do numero :"))
+
+print("La suma es ",x+y)
+print("La resta es ",x-y)
+print("La multiplicacion es ",x*y)
+print("La division es ",x/y)
+print("La division entera es", x//y)
