69dc395ad238676723c17bfd4bc9126b94e4a276
SBrisseth <110753585+SBrisseth@users.noreply.github.com>
2023-01-15 14:04:35 -0500

    subir preg_7.py a github

---
+++ preg_7.py
@@ -0,0 +1,7 @@
+a = float(input("Ingrese el 1er valor:"))
+b = float(input("Ingrese el 2do valor:"))
+
+print("Los numeros son iguales ",a==b)
+print("Los numeros son diferentes", a!=b)
+print("El primero es mayor que el segundo",a>b)
+print("El segundo es mayor o igual que el primero",b>=a)
